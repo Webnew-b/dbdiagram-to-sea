f6a642be67330fdbeabf7f22e331b652ef587318
lexon <lexonnewb@foxmail.com>
2025-05-15 15:32:02 +0800

    Feat: Implement SchemaError for validation

--- src/error_enum/schema_error.rs
+++ src/error_enum/schema_error.rs
@@ -11,5 +11,21 @@ pub enum SchemaErrorKind {
     #[error("Input column type {colum_type} doesn't be contained.")]
     NoContained {
         colum_type:String,
-    }
+    },
+
+    #[error("Fail to get configuration from content.")]
+    FileFormatFile,
+
+
+    #[error("Could not match pattern from : {match_by}")]
+    RegexMatchError {
+        match_by:String
+    },
+
+    #[error("Validation Error:Value: {field},Type:{field_type}")]
+    VailateFieldFail {
+        field:String,
+        field_type:String,
+    },
+    
 }
